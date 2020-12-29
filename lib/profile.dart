ec88967138073962ddcbdb66bc519b3ede9bd791
A-N-uraag <marapallyanuraag@gmail.com>
2020-12-29 22:16:22 +0530

    Basic UI done

---
+++ lib/profile.dart
@@ -0,0 +1,44 @@
+import 'package:ProjectLocus/utils/namecard.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfileView(),
+    );
+  }
+}
+
+class ProfileView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          NameCard(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FloatingActionButton.extended(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              label: Text("Private Mode"),
+              icon: Icon(Icons.privacy_tip),
+              backgroundColor: Color(0xff33ffcc),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        label: Text("Edit"),
+        icon: Icon(Icons.edit),
+        backgroundColor: Color(0xff33ffcc),
+      ),
+    );
+  }
+}
---
+++ lib/utils/namecard.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NameCard extends StatelessWidget {
+  const NameCard({
+    Key key,
+  })  : 
+        super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        color: Colors.black,
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: Container(
+            width: MediaQuery.of(context).size.width / 1.1,
+            height: MediaQuery.of(context).size.height / 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.person, color: Color(0xff33ffcc),),
+                Text("User 101"),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children:[Icon(Icons.email, size: 20,), Text(" 101@usermail.com")]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(children: [Icon(Icons.phone, size: 20), Text(" 9999999999")]),
+                    Row(children: [Icon(Icons.cake, size: 20), Text(" 01-01-0000")]),
+                ]),
+                Text("This is User's bio")
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
