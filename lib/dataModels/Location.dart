8b55f24f4a6680a2bb4ce93fb1c6da433dd15c40
anuragreddy2000 <anuragreddy1000@gmail.com>
2020-12-29 21:18:32 +0530

    Implemented the DBUtils class as well as data models for profiles and locations

---
+++ lib/dataModels/Location.dart
@@ -0,0 +1,9 @@
+class Location{
+  String email;
+  double latitude;
+  double longitude;
+  String time;
+  String date;
+
+  Location(this.email,this.latitude,this.longitude,this.time,this.date);
+}
